0958cd1ab8422af2d96b29c53ab6dee968860ffc
Haiwen Diao <37319709+Paranioar@users.noreply.github.com>
2023-10-20 11:44:30 +0800

    Add files via upload

---
+++ visualize.py
@@ -0,0 +1,16 @@
+"""
+# Please refer to https://github.com/Paranioar/RCAR for related visualization code.
+# It now includes visualize_attention_mechanism, visualize_similarity_distribution, visualize_rank_result, and etc.
+
+# I will continue to update more related visualization codes when I am free.
+# If you find these codes are useful, please cite our papers and star our projects. (We do need it! HaHaHaHa.)
+# Thanks for the interest in our projects.
+"""
+
+
+
+
+
+
+
+
